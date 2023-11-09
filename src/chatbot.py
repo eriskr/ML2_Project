efe82ceaafb30714ff00138377203337a134824d
Erik <eskram2001@gmail.com>
2023-11-09 13:25:41 +0100

    .

--- src/chatbot.py
+++ src/chatbot.py
@@ -1,28 +1,60 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, jsonify
 import openai
-
-from dotenv import load_dotenv
 import os
+from dotenv import load_dotenv
 
-load_dotenv()  # This loads the .env file and its variables
+# Load environment variables
+load_dotenv()
 
-openai.api_key = os.getenv('OPENAI_API_KEY')
-
+# Initialize the Flask application
 app = Flask(__name__)
 
+# Load the OpenAI API key from the environment variable
+openai.api_key = os.getenv('OPENAI_API_KEY')
+
+# Confirm the API key is present
+if not openai.api_key:
+    raise ValueError("No OpenAI API key found. Set the OPENAI_API_KEY environment variable.")
+
+# Route to serve the HTML page
 @app.route("/")
 def index():
-    return render_template('index.html')  # You would create a corresponding HTML file for the chat interface.
+    return render_template('index.html')
 
+# Route to handle sending messages to OpenAI and getting the response
 @app.route("/ask", methods=["POST"])
 def ask():
-    user_message = request.form['message']
-    response = openai.Completion.create(
-      engine="davinci",
-      prompt=user_message,
-      max_tokens=150
-    )
-    return response.choices[0].text.strip()
+    try:
+        # Get the user message from the form data
+        user_message = request.form['message']
 
+        # Prepare the prompt for the OpenAI API call
+        prompt = (f"William Shakespeare is a chatbot designed to simulate a conversation with the historical figure. "
+                  f"Shakespeare will respond to your questions in the first person as if he were alive today.\n\n"
+                  f"You: {user_message}\nShakespeare:")
+
+        # Make the API call to OpenAI using the new API
+        response = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=[
+                {"role": "system", "content": "You are William Shakespeare."},
+                {"role": "user", "content": user_message},
+            ]
+        )
+
+        # Extract the response text
+        bot_response = response['choices'][0]['message']['content']
+
+        # Return the bot response as JSON
+        return jsonify(bot_response)
+
+    except Exception as e:
+        # Log the exception message
+        app.logger.error(f'An error occurred: {e}')
+
+        # Return a JSON error message
+        return jsonify({'error': 'An error occurred while processing your request.'}), 500
+
+# Run the Flask app
 if __name__ == "__main__":
     app.run(debug=True)
--- src/openai-test.py
+++ src/openai-test.py
@@ -1,12 +1,28 @@
-from openai import OpenAI
-client = OpenAI()
+import openai
+from dotenv import load_dotenv
+import os
 
-completion = client.chat.completions.create(
-  model="gpt-3.5-turbo",
-  messages=[
-    {"role": "system", "content": "You are a poetic assistant, skilled in explaining complex programming concepts with creative flair."},
-    {"role": "user", "content": "Compose a poem that explains the concept of recursion in programming."}
-  ]
-)
+# Load environment variables from a .env file
+load_dotenv()
 
-print(completion.choices[0].message)
+# Set the API key from the OPENAI_API_KEY environment variable
+openai.api_key = os.getenv('OPENAI_API_KEY')
+
+# Ensure the API key is present
+if not openai.api_key:
+    raise ValueError("The OPENAI_API_KEY environment variable is not set.")
+
+# The prompt for the test
+prompt_text = "Translate the following English text to French: 'Hello, how are you?'"
+
+# Call the OpenAI API
+try:
+    response = openai.Completion.create(
+        engine="text-davinci-003",  # Or "gpt-3.5-turbo" depending on your plan
+        prompt=prompt_text,
+        max_tokens=60
+    )
+    # Print the response
+    print(response.choices[0].text.strip())
+except Exception as e:
+    print(f"An error occurred: {e}")

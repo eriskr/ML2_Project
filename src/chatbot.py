6187ff85476b07d6b3b1a287ad3afa58e1474c49
Erik <eskram2001@gmail.com>
2023-11-10 16:11:23 +0100

    Frontend + api now working

--- src/chatbot.py
+++ src/chatbot.py
@@ -16,28 +16,39 @@ openai.api_key = os.getenv('OPENAI_API_KEY')
 if not openai.api_key:
     raise ValueError("No OpenAI API key found. Set the OPENAI_API_KEY environment variable.")
 
+# Global variable to store the current historical figure
+current_historical_figure = None
+
 # Route to serve the HTML page
 @app.route("/")
 def index():
+    global current_historical_figure
+    current_historical_figure = None  # Reset at the start of each session
     return render_template('index.html')
 
 # Route to handle sending messages to OpenAI and getting the response
 @app.route("/ask", methods=["POST"])
 def ask():
+    global current_historical_figure
     try:
-        # Get the user message from the form data
-        user_message = request.form['message']
+        data = request.get_json()
+        user_message = data['message']
+
+        # Check if setting a new historical figure
+        if not current_historical_figure:
+            current_historical_figure = user_message
+            return jsonify(f"You are now talking to {current_historical_figure}. What would you like to ask?")
 
         # Prepare the prompt for the OpenAI API call
-        prompt = (f"William Shakespeare is a chatbot designed to simulate a conversation with the historical figure. "
-                  f"Shakespeare will respond to your questions in the first person as if he were alive today.\n\n"
-                  f"You: {user_message}\nShakespeare:")
+        prompt = (f"{current_historical_figure} is a chatbot designed to simulate a conversation with the historical figure. "
+                  f"They will respond to your questions as if they were alive today.\n\n"
+                  f"You: {user_message}\n{current_historical_figure}:")
 
-        # Make the API call to OpenAI using the new API
+        # Make the API call to OpenAI
         response = openai.ChatCompletion.create(
             model="gpt-3.5-turbo",
             messages=[
-                {"role": "system", "content": "You are William Shakespeare."},
+                {"role": "system", "content": f"You are {current_historical_figure}."},
                 {"role": "user", "content": user_message},
             ]
         )
